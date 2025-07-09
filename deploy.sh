278a72bd3d66b7ec1114054fad58f7d64479fe1a
Francis Akpotu <akpotu.francis@outlook.com>
2025-07-08 23:31:29 -0400

    Tested and approved by client QA team

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,11 @@ database connection
 db-password=$password
 db-username=$user
 more db
+deploy java application
+Deploy python apps
+deploy in the dev env
 deployment5 master
 deplyment4
+deploy to staging env
+echo good job
+Tested and accepted
